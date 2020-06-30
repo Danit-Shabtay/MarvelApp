ed28a8c22b9d6c85c974195548ef65cdffe0768a
Danit-Shagan <danishagan@gmail.com>
2020-06-30 23:14:53 +0300

    Test git

--- Project1My first app in CPP/MyForm.h
+++ Project1My first app in CPP/MyForm.h
@@ -222,6 +222,7 @@ namespace Project1MyfirstappinCPP {
 	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
 		
 	}
+	// test git
 	private: System::Void btnContinue_Click(System::Object^ sender, System::EventArgs^ e) {
 		System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(MyForm::typeid));
 		this->pictureBox1->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"venum")));
